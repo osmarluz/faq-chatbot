f50368cffbb94db240ad8b5e4275216c07800e64
osmarluz <osmar.luz@gmail.com>
2017-10-13 21:01:45 -0300

    Configure database for production deployment and create seeds

--- config/database.rb
+++ config/database.rb
@@ -7,7 +7,7 @@ configure :test do
     username: 'postgres',
     host: 'postgres'
   }
- end
+end
  
 configure :development do
   set :database, {
@@ -18,15 +18,17 @@ configure :development do
     username: 'postgres',
     host: 'postgres'
   }
- end
+end
  
 configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/faq-chatbot_production')
+  
   set :database, {
-    adapter: 'postgresql',
-    encoding: 'utf8',
-    database: 'faq-chatbot_production',
-    pool: 5,
-    username: 'postgres',
-    host: 'postgres'
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
   }
- end
+end
